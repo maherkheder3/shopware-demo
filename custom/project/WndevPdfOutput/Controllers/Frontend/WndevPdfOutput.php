7f8a2c8b57bdc9341fe2704dbed27fcb0afc0381
Maher Kheder <kheder@web-netz.de>
2019-02-14 16:44:02 +0100

    get Article from Database

--- custom/project/WndevPdfOutput/Controllers/Frontend/WndevPdfOutput.php
+++ custom/project/WndevPdfOutput/Controllers/Frontend/WndevPdfOutput.php
@@ -7,7 +7,25 @@ class Shopware_Controllers_Frontend_WndevPdfOutput extends Enlight_Controller_Ac
 {
     public function indexAction()
     {
-        // Assign a template variable
-        $this->View()->assign('name', 'WndevPdfOutput');
+//        $this->Response()->setHeader('Content-type', 'application/pdf', true); // application/pdf
+        $id = $this->request->get('id');
+        $modules = $this->container->get('modules');
+
+        try {
+            $sArticle = $modules->Articles()->sGetArticleById($id);
+
+            if (!$sArticle) {
+                throw new \InvalidArgumentException(sprintf('article "%s" not found', $id));
+            }
+
+//            var_dump($sArticle);
+            $this->View()->assign('sArticle', $sArticle);
+
+        } catch (\Exception $exception) {
+            var_dump($exception->getMessage());
+            die();
+            $this->redirect(['controller' => 'index']);
+        }
+
     }
 }
